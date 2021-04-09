c313f79c42b0e5b21838a96384af942263b560a8
elijah-lozano <elijahlozano@hotmail.com>
2021-04-09 16:11:48 -0500

    converted browse therapists activity to fragment & connected to nav bar

--- app/src/main/java/com/example/circle/ui/therapists/TherapistsFragment.java
+++ app/src/main/java/com/example/circle/ui/therapists/TherapistsFragment.java
@@ -11,25 +11,43 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.circle.R;
+import com.example.circle.TherapistsAdapterTemp;
 
 public class TherapistsFragment extends Fragment {
 
-    private TherapistsViewModel therapistsViewModel;
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.amber_bailey,
+            R.drawable.shawndel_springer,
+            R.drawable.bob_stead,
+            R.drawable.stephen_garcia,
+            R.drawable.deborah_lane,
+            R.drawable.april_boreham,
+            R.drawable.george_cox,
+            R.drawable.ashley_logan,
+            R.drawable.richard_walla,
+            R.drawable.sandy_zamora};
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        therapistsViewModel =
-                new ViewModelProvider(this).get(TherapistsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_therapists, container, false);
-        final TextView textView = root.findViewById(R.id.text_therapists);
-        therapistsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        View view = inflater.inflate(R.layout.recycler_view, container, false);
+
+        recyclerView = view.findViewById(R.id.therapists_recyclerView);
+
+        s1 = getResources().getStringArray(R.array.therapist_profiles);
+        s2 = getResources().getStringArray(R.array.therapist_descriptions);
+
+        TherapistsAdapterTemp therapistsAdapterTemp = new TherapistsAdapterTemp(getContext(), s1, s2, images);
+        recyclerView.setAdapter(therapistsAdapterTemp);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
     }
 }
