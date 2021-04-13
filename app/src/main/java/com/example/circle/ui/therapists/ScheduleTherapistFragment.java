4ad407ba1526293624098768f0de088752906d06
elijah-lozano <elijahlozano@hotmail.com>
2021-04-13 17:12:46 -0500

    got request button working

---
+++ app/src/main/java/com/example/circle/ui/therapists/ScheduleTherapistFragment.java
@@ -0,0 +1,25 @@
+package com.example.circle.ui.therapists;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.circle.R;
+
+
+public class ScheduleTherapistFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_therapist_schedule, container, false);
+
+
+
+        return view;
+    }
+}
