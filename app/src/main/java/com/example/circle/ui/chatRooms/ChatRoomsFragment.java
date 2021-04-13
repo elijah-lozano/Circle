bab2d8d996bc32f773cfc51386f456829d13f05a
Dsoeung <47315471+Dsoeung@users.noreply.github.com>
2021-04-13 02:01:51 -0500

    test

--- app/src/main/java/com/example/circle/ui/chatRooms/ChatRoomsFragment.java
+++ app/src/main/java/com/example/circle/ui/chatRooms/ChatRoomsFragment.java
@@ -17,19 +17,11 @@ import com.example.circle.R;
 public class ChatRoomsFragment extends Fragment {
 
     private ChatRoomsViewModel chatRoomsViewModel;
+    //{
+    //public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      //                       Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_first, container, false);
+    //}
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        chatRoomsViewModel =
-                new ViewModelProvider(this).get(ChatRoomsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_chat_rooms, container, false);
-        final TextView textView = root.findViewById(R.id.text_chat_rooms);
-        chatRoomsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
 }
